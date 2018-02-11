e2bb7016feb1eba728f8daa1b6fe006135981252
sheblaev <sheblaev@gmail.com>
2018-02-11 12:40:34 +0300

    some bit hacks

--- main.cc
+++ main.cc
@@ -3,7 +3,35 @@
 #include <cassert>
 using namespace std;
 #define LUCAL
+#if 0
+/** bit masks */
+const uintmax_t
+ALL_ONES = ~(uintmax_t)0,
+         O_ONES = ALL_ONES/0xff, //0x...10101
+         FIVES  = ALL_ONES/3,    //0x...33333
+         THREES = ALL_ONES/5,    //0x...55555
+         OFOFS  = ALL_ONES/17;   //0x...F0F0F
 
+/** return number of bits set to one in <code>bits</code> */
+// fast multiplication variant
+int popCount(uintmax_t bits) {
+        bits -= (bits >> 1) & FIVES;
+        bits = (bits & THREES) + ((bits >> 2) & THREES);
+        // of course, O7O7s would do - but 255 doesn't divide by 7
+        bits = bits + (bits >> 4) & OFOFS;
+        // needs one more "fold operation" and "ones spaced 16"
+        //  for 256 <= #bits < 65536
+        return (bits * O_ONES) >> (sizeof bits * 8);
+}
+
+/** return (odd) parity of <code>bits</code> */
+int parity(uintmax_t bits) {
+        uintmax_t shifted;
+        for (int shift = 4 ; 0 < ((shifted = bits) >>= shift) ; shift *= 2)
+                bits ^= shifted;
+        return (0x6996 >> ((int)bits & 0xf)) & 1;
+}
+#endif
 
 
 unsigned int INT_TO_GRAY (int X){
@@ -108,6 +136,7 @@ template< size_t N > class GRAY_INT {
                         r.SetBits(res.to_ulong());
                         return r;
 #else
+#ifdef JAPAN
                         bool v0 = false;
                         bool v1 = false;
                         bool prev_bit = 0;
@@ -158,7 +187,22 @@ template< size_t N > class GRAY_INT {
                         r.SetBits(res.to_ulong());
                         return r;
 
-
+#else
+                        Unsigned bit = 1;
+                        for ( ; bit <= rhs && bit != 0 ; bit <<= 1) {
+                                long both = lhs_p&rhs_p;
+                                res ^= both;
+                                lhs_p = (lhs_p ^ both ^ lhs) << 1;
+                                rhs_p = ((rhs_p ^ both ^ rhs) & bit) << 1;  
+                        }
+                        if (0 == rhs_p)
+                                return res;
+                        lhs_p &= bit;
+                        if (0 != lhs_p)
+                                return res ^= lhs_p;
+                        bit = lhs ^ (lhs & (lhs-bit));
+                        return res ^ (bit << 1);
+#endif
 #endif
                 }
 
